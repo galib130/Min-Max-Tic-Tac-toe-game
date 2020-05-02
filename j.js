9a3069035740f126fcc45482499d608389016603
galib130 <galibshams16@gmail.com>
2020-05-02 18:24:54 +0600

    initial commit

---
+++ j.js
@@ -0,0 +1,233 @@
+// var header=document.querySelector("h1")
+// var head1=document.querySelector("#pick")
+// var head2=document.querySelector("#pick2")
+// var head3=document.querySelector("#pick3")
+
+var player=document.getElementById("crowd");
+// player.loop="true";
+
+
+// function random()
+// {
+//   var letters="0123456789ABCDEF"
+//   var color='#';
+//   for(var i=0;i<6;i++)
+//   {
+//     color+=letters[Math.floor(Math.random()*16)];
+//
+//   }
+//   return color
+// }
+
+// function changeHeader()
+// {
+// colorInput=random()
+// header.style.color=colorInput;
+//  }
+//  setInterval("changeHeader()",200);
+//
+// head1.addEventListener("mouseover",function(){
+//
+//   head1.textContent="Mouse";
+//
+// })
+
+// head1.addEventListener("mouseout",function(){
+//   head1.textContent='First';
+// })
+
+
+
+var s=[10];
+s[0]=0;
+ s[1]=document.querySelector("#im1");
+ s[2]=document.querySelector("#im2");
+ s[3]=document.querySelector("#im3");
+ s[4]=document.querySelector("#im4");
+ s[5]=document.querySelector("#im5");
+  s[6]=document.querySelector("#im6");
+ s[7]=document.querySelector("#im7");
+ s[8]=document.querySelector("#im8");
+ s[9]=document.querySelector("#im9");
+ // s[10]=document.querySelector('#im11');
+ var r1=document.querySelector("#Red");
+ var b1=document.querySelector("#Blue");
+var w=[10];
+ for(var l=1;l<=9;l++)
+ {
+   w[l]=document.querySelector('#im1'+l);
+ }
+// s1.bgColor="orange";
+var k=2;
+
+  var pi=[10];
+  for(var j=1;j<=9;j++)
+  {
+    pi[j]=document.querySelector("#f"+j);
+  }
+  var bool;
+  var ht=0;
+  var cs=0;
+  var but1=document.querySelector("#button1");
+  var html=document.querySelector(".html");
+function result(c)
+{
+  for(var y=0;y<8;y++)
+  {
+    var flag=0;
+    for(var z=0;z<3;z++)
+    {
+      if(mem[winner[y][z]]==c)
+      {
+        flag++;
+      }
+    }
+        console.log(flag);
+    if(flag==3)
+    {
+      if(c==3){console.log("red wins");
+      r1.style.color="red";
+      r1.style.marginLeft="42%";
+      r1.textContent="HTML WINS" //1 second
+
+      var delayInMilliseconds = 3000;
+setTimeout(function() {
+  //your code to be executed after 1 second
+
+var vic=document.querySelector("#victory");
+// r1.style.marginRight="40%";
+r1.style.marginLeft="38%";
+  r1.textContent="FLAWLESS VICTORY";
+}, delayInMilliseconds);
+var seconds = 12000;
+setTimeout(function() {
+//your code to be executed after 1 second
+
+var vic=document.querySelector("#victory");
+vic.play();
+
+},seconds);
+      ht++;
+      player.play();
+      html.textContent="HTML:"+ht+ " "+"CSS:"+cs;
+      r1.style.visibility="visible";
+      but1.textContent="Play Again?";
+      break;}
+      else if(c==4)
+      {console.log("blue wins");
+r1.style.color="blue";
+  r1.style.marginLeft="43%";
+      r1.textContent="CSS WINS";
+var vic=document.querySelector("#victory");
+      var delayInMilliseconds = 3000;
+      setTimeout(function() {
+        //your code to be executed after 1 second
+          r1.style.marginLeft="38%";
+        r1.textContent="FLAWLESS VICTORY";
+
+      }, delayInMilliseconds);
+      var seconds = 12000;
+      setTimeout(function() {
+      //your code to be executed after 1 second
+
+      var vic=document.querySelector("#victory");
+      vic.play();
+
+      },seconds);
+      r1.style.visibility="visible";
+      but1.textContent="Play Again?";
+      player.play();
+      cs++;
+        html.textContent="HTML:"+ht+ " "+"CSS:"+cs;
+
+      break;
+      }
+
+    }
+    else {
+
+      flag=0;
+
+    }
+  }
+}
+
+
+var mem=[2,2,2,2,2,2,2,2,2];
+var job=[2,2,2,2,2,2,2,2,2,2];
+var winner=[[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
+const num=[1,2,3,4,5,6,7,8,9];
+num.forEach(i=>{
+    pi[i].addEventListener("click",function(){ var t=s[i];
+   if(k%2==0&&job[i]==2){
+
+       t.style.display="block";
+       var audio=document.getElementById("a1");
+       audio.play();
+       k++;
+       mem[i]=3;
+       job[i]=3;
+       result(3);
+
+
+
+  }
+    else if(k%2!=0 &&job[i]==2) {
+
+  var audio=document.getElementById("a1");
+  audio.play();
+  var a=w[i];
+    t.style.display="none";
+    a.style.display="block";
+
+    k++;
+    mem[i]=4;
+    job[i]=4;
+    result(4);
+
+    }
+  })
+})
+
+
+
+
+but1.addEventListener("click",function(){
+num.forEach(i => {
+  var cross=s[i];
+  var dot=w[i];
+  cross.style.display="none";
+  dot.style.display="none";
+  k=0;
+  mem[i]=2;
+  job[i]=2;
+    r1.style.visibility="hidden";
+});
+
+// player.pause();
+})
+
+
+
+var flag=0;
+var j=0;
+for(var i=0;i<8;i++)
+{
+
+   if(mem[winner[i][j]]==mem[winner[i][j+1]]&&mem[winner[i][j]]==mem[winner[i][j+2]] && mem[i][j]!=2)
+   {
+   console.log("Game Over");
+   if(mem[winner[i][j]]=='3')
+   {
+     console.log("Html wins");
+   }
+   if(mem[winner[i][j]]=='4')
+   {
+     console.log("CSS wins");
+   }
+   mem=[];
+
+
+   }
+
+}
